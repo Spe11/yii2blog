d1dd6f98158b801eee82923918e5ca0dc7661b2f
Spe11 <31505821+Spe11@users.noreply.github.com>
2018-04-18 16:21:46 +0400

    commit

--- config/db.php
+++ config/db.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    /* 'class' => 'yii\db\Connection',
+    'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=johnny.heliohost.org;dbname=spe11_blogdb',
     'username' => 'spe11_user',
     'password' => 'password',
-    'charset' => 'utf8', */
+    'charset' => 'utf8',
 
-    'class' => 'yii\db\Connection',
+     /* 'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=blogdb',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8'
+    'charset' => 'utf8'  */
 ];
