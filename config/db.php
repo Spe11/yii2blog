bc481ed905026acd5bff26c6b5532eb614f9bddd
Spe11 <31505821+Spe11@users.noreply.github.com>
2018-04-19 15:51:10 +0400

    deploy

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=blogdb',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=bevhg2djq-mysql.services.clever-cloud.com;dbname=bevhg2djq',
+    'username' => 'u2vbb3kfho0mvlyu',
+    'password' => '5WTYgCzkYTL5jK6xWH1',
     'charset' => 'utf8'
 ];
